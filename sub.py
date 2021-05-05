85f9b3e6b5f0ef75f1f6d851c30bc46b79a3a1a2
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-05 11:09:27 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,4 +1,4 @@
-
+import subprocess, shlex
 def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
