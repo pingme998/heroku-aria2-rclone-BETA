25cc3ec432c8670cefc7cfc62d68e19e4a81c217
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-05 10:41:39 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -2,8 +2,7 @@ import time
 import subprocess
 
 p1=subprocess.run(["sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;' "])
-p2=subprocess.run(["aria2c --dir=/home --enable-rpc=true --rpc-allow-origin-all=true --rpc-listen-all=true --rpc-listen-port=6800
-"])
+p2=subprocess.run(["aria2c --dir=/home --enable-rpc=true --rpc-allow-origin-all=true --rpc-listen-all=true --rpc-listen-port=6800"])
 
     p1.start()
     p2.start()
