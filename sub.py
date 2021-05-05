2838decf329e385aefd6c052f69694a6a57f38bf
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-05 11:18:22 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -2,7 +2,6 @@ import subprocess, shlex
 def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
-    print proc_stdout
 
 subprocess_cmd('aria2c --dir=/home --enable-rpc=true --rpc-allow-origin-all=true --rpc-listen-all=true --rpc-listen-port=6800
 ; sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf')
