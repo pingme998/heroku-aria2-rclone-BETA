6007066b4b86412d25032afe2bca1f83ae1d2ac2
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-09 01:31:37 +0530

    Update entrypoint.sh

--- Essential-Files/entrypoint.sh
+++ Essential-Files/entrypoint.sh
@@ -13,9 +13,9 @@ my_var="${my_var#, }"              
 echo "$my_var"
 
 OLD=REMOTCLOUD
-sed "s|$OLD|$my_var|g" /Essential-Files/Aria2Rclone.ipynb>Aria2Rclone.ipynb
+sed "s|$OLD|$my_var|g" /Aria2Rclone.htpy >/Aria2Rclone.ipynb
 
 jupyter nbextension enable --py widgetsnbextension --sys-prefix
 jupyter serverextension enable voila --sys-prefix
 curl 'https://raw.githubusercontent.com/developeranaz/Aria2-Rclone-Remote-Uploader-HEROKU/main/Essential-Files/d/donate.developeranaz.txt'
-voila --port=$PORT --no-browser  Aria2Rclone.ipynb --theme=dark
+voila --port=$PORT --no-browser  /Aria2Rclone.ipynb --theme=dark
