469c097e884c9b0128d484af3ad309ce8944ac69
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-08 21:16:48 +0530

    Delete sub.py

--- sub.py
+++
@@ -1,7 +0,0 @@
-import subprocess, shlex
-def subprocess_cmd(command):
-    process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
-    proc_stdout = process.communicate()[0].strip()
-
-subprocess_cmd('aria2c --dir=/home --enable-rpc=true --rpc-allow-origin-all=true --rpc-listen-all=true --rpc-listen-port=6800
-; sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf')
